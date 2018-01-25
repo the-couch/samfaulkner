44210a1a8c8b72045d61aa9b812aad5221434d60
iamkevingreen <kevin@thecouch.nyc>
2018-01-25 15:13:05 -0500

    switch to flickity

--- src/templates/index.js
+++ src/templates/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import LazyLoad from 'react-lazyload'
 
 export default class Index extends React.Component {
   render () {
@@ -26,6 +27,7 @@ class Home extends React.Component {
     const fl = new Flickity(slideshow, {
       contain: true,
       lazyLoad: true,
+      pageDots: false,
       prevNextButtons: false,
       wrapAround: true,
       ease:  'cubic-bezier(0.075, 0.82, 0.165, 1)'
@@ -34,29 +36,27 @@ class Home extends React.Component {
     // Lory listeners
 
     const slideShowJazz = (e) => {
-      console.log(e)
-      const currentSlide = e.detail.currentSlide
+      console.log(fl.selectedIndex)
 
-      console.log('wow', currentSlide)
-      self.setState({currentSlide: currentSlide})
+      self.setState({currentSlide: fl.selectedIndex})
     }
 
-    slideshow.addEventListener('after.lory.slide', slideShowJazz)
+    fl.on('select', () => slideShowJazz())
 
     let animate = true
 
     const debounce = (func, wait = 80) => {
       let timeout
       return (...args) => {
-        clearTimeout(timeout);
+        clearTimeout(timeout)
         if (animate) {
-          func.apply(this, args);
+          func.apply(this, args)
           animate = false
         }
         timeout = setTimeout(() => {
           animate = true
-        }, wait);
-      };
+        }, wait)
+      }
     }
 
     const animateSlide = (e) => {
@@ -90,7 +90,9 @@ class Home extends React.Component {
                     <div className='slide__text'>
                       <h1>{study.node.name}</h1>
                     </div>
-                    <img src={study.node.image.file.url} />
+                    <div className='slide__bg' style={{backgroundImage: 'url(' + study.node.image.file.url + ')' }}>
+                      <img src={study.node.image.file.url} />
+                    </div>
                   </div>
                 </div>
               </li>
